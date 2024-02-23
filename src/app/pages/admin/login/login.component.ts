18dfbd501cb1fde7f59e6139bffd32dfec5f5a9e
leassantos <lsts2005@gmail.com>
2024-02-22 22:08:53 -0300

    feature: create Login databind and router to products page

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginObj: any = {
+    username: '',
+    password: ''
+  };
+
+  constructor(private router: Router){}
+
+  onLogin(){
+    if(this.loginObj.username == 'admin' && 
+      this.loginObj.password == '123456'){
+        this.router.navigateByUrl('/products')
+    }else{
+      alert('Wrong Credentials')
+    }
+  }
 }

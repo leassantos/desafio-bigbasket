8a2608d71814efc93151b913c643c09c8b5162d5
leassantos <lsts2005@gmail.com>
2024-02-26 22:06:52 -0300

    feature: create crud method sign

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -6,13 +6,43 @@ import { Injectable } from '@angular/core';
 })
 export class ProductService {
 
+
   constructor(private http: HttpClient) { }
+
 
   getAllProducts(){
 
   }
 
-  getCategory(){
+  createProduct(){
+
+  }
+
+  deleteProduct(){
     
   }
+
+  updateProduct(){
+
+  }
+
+  getCategory(){
+    this.http.get('assets/categories.json').subscribe(data => {
+      console.log('Funcionaou: ' + data)
+    })
+  }
+}
+
+const CATEGORY_DATA: Category[] = [
+  {categoryId: 1, categoryName: '', parentCategoryId: 1},
+  {categoryId: 1, categoryName: '', parentCategoryId: 1},
+  {categoryId: 1, categoryName: '', parentCategoryId: 1},
+  {categoryId: 1, categoryName: '', parentCategoryId: 1},
+  {categoryId: 1, categoryName: '', parentCategoryId: 1},
+]
+
+export interface Category{
+  categoryId: number;
+  categoryName: string;
+  parentCategoryId: number;
 }

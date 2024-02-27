a2f2e1dc317a9e4a2c4f315a0380162b72414969
leassantos <lsts2005@gmail.com>
2024-02-26 21:07:26 -0300

    feature: create product component

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './domain/product';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../services/product/product.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
 
+  isSidePanelVisible: boolean = false;
+  
+  product: Product = {
+    productId: 0,
+    productSku: '',
+    productName: '',
+    productPrice: 0,
+    productShortName: '',
+    productDescription: '',
+    createdDate: new Date(),
+    deliveryTimeSpan: '',
+    categoryId: 0,
+    productImageUrl: '',
+  };
+
+  constructor(private service: ProductService){}
+
+  ngOnInit(): void {
+    this.getAllCategory();
+  }
+
+  getAllCategory(){
+
+  }
+
+  onSidePanel(){
+    this.isSidePanelVisible = true;
+  }
+
+  closeSidePanel(){
+    this.isSidePanelVisible = false;
+  }
 }
